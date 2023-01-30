5739099a07b7ff0486d25e58e2bed1f308aab4a3
Karthik Alakunta <karthikalakunta21@gmail.com>
2023-01-30 23:02:31 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Text} from "@chakra-ui/react";
 function Footer() {
   return (
     <div>
-      <Text mb="10px" textAlign={"center"}>Designed and build by <span style={{fontWeight:"bold"}}>KARTHIK ALAKUNTA</span>, @ 2022 All rights reserved</Text>
+      <Text mb="10px" textAlign={"center"}>Designed and build by <span style={{fontWeight:"bold"}}>KARTHIK ALAKUNTA</span>, @ 2023 All rights reserved</Text>
     </div>
   );
 }
